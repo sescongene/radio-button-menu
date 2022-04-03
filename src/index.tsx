eae7e60ba1ee5b8decd75a6682ef99481c91bd14
Gene Sescon <sescongene.dev@gmail.com>
2022-04-03 23:44:20 +0800

    Fix render warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import { render } from 'react-dom';
-import App from './pages/App';
+import { createRoot } from "react-dom/client";
+import App from "./pages/App";
 
-render(<App />, document.getElementById('root'));
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+root.render(<App />);
